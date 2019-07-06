ac8f3ef7fb4eda9f62d68dc9d57b3bab3db609f9
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-07-06 21:02:28 +0900

    Particle

--- core/src/main/scala/com/yuiwai/yachiyo/core/Particle.scala
+++ core/src/main/scala/com/yuiwai/yachiyo/core/Particle.scala
@@ -1,6 +1,9 @@
 package com.yuiwai.yachiyo.core
 
 final case class Particle[T: Amount](pos: Pos[T])
+final case class ParticleSystem[T: Amount](pos: Pos[T], particles: Seq[Particle[T]]) {
+
+}
 
 final case class Gravity(value: Double) extends AnyVal
 
---
+++ core/src/test/scala/com/yuiwai/yachiyo/core/ParticleSpec.scala
@@ -0,0 +1,9 @@
+package com.yuiwai.yachiyo.core
+
+import utest._
+
+class ParticleSpec extends TestSuite {
+  val tests = Tests {
+
+  }
+}

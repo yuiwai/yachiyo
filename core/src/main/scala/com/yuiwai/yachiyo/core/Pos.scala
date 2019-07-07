ffa022431348bec5ccc022b2458466d6c47726a6
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-07-07 21:25:14 +0900

    Particle Generatorまで実装

--- core/src/main/scala/com/yuiwai/yachiyo/core/Pos.scala
+++ core/src/main/scala/com/yuiwai/yachiyo/core/Pos.scala
@@ -1,4 +1,8 @@
 package com.yuiwai.yachiyo.core
 
-final case class Pos[T: Amount](x: T, y: T) {
+final case class Pos[T: Amount](x: T, y: T) extends Vector2D[T] {
+  def +(that: Vector2D[T]): Pos[T] = copy(amount.+(x, that.x), amount.+(y, that.y))
+}
+object Pos {
+  def zero[T](implicit a: Amount[T]): Pos[T] = apply(a.zero, a.zero)
 }
--- core/src/test/scala/com/yuiwai/yachiyo/core/ParticleSpec.scala
+++ core/src/test/scala/com/yuiwai/yachiyo/core/ParticleSpec.scala
@@ -2,8 +2,41 @@ package com.yuiwai.yachiyo.core
 
 import utest._
 
-class ParticleSpec extends TestSuite {
+object ParticleSpec extends TestSuite {
   val tests = Tests {
+    val system = ParticleSystem[Int](
+      Pos.zero,
+      Seq.empty,
+      Generator(_ => Particle.zero[Int], 10, 1))
+    "spawn" - {
+      system.spawn().pos ==> Pos.zero[Int]
+    }
+    "generator" - {
+      Generator[Unit, Int](_ => 1, 1, 0).generate(()) match {
+        case (generated, _) => generated.size ==> 0
+      }
+      Generator[Unit, Int](_ => 1, 1, 10).generate(()) match {
+        case (generated, _) => generated.size ==> 10
+      }
+    }
+    "accelerated" - {
+      val p1 = Particle.zero[Int]
+      p1.speed ==> Speed.zero[Int]
+      p1.accelerated(Force(1, 2)).updated().speed ==> Speed(1, 2)
+    }
+    "updated" - {
+      val p1 = Particle[Int](Pos.zero, Speed.zero)
+      p1.updated() ==> p1
+      p1.accelerated(Force(1, 2)).updated() match {
+        case Particle(p, _) => p ==> Pos(1, 2)
+      }
+    }
+    "angle" - {
+      Speed(1, 1) * Angle.fromDegree(0) ==> Speed(1, 1)
+      Speed(1, 0) * Angle.fromDegree(90) ==> Speed(0, 1)
+    }
+    "lifetime" - {
 
+    }
   }
 }

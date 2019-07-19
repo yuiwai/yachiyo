7c9ddeefb3874bdadee94533b7fd23d9ca8ef672
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-07-20 08:18:55 +0900

    Blockにメソッド追加中

--- core/src/main/scala/com/yuiwai/yachiyo/core/Block.scala
+++ core/src/main/scala/com/yuiwai/yachiyo/core/Block.scala
@@ -1,14 +1,50 @@
 package com.yuiwai.yachiyo.core
 
 trait Block[T] {
+  type P = Pos[Int]
   def width: Int
   def height: Int
   def size: Int = values.size
+  def posToIndex(pos: P): Option[Int] = Some(pos.x + pos.y * width).filter(_ < size)
+  def indexToPos(index: Int): P = Pos(index % width, index / width)
   val values: Seq[T]
+  def valuesWithPos: Seq[(P, T)] = values.zipWithIndex.map { case (v, i) => indexToPos(i) -> v }
+  def map[U](f: T => U): Block[U]
   def iterator: BlockIterator[T] = new BlockIterator[T](this, 0)
   def foreach(f: T => Unit): Unit = values.foreach(f)
+  def row(index: Int): Option[Row[T]]
+  def region(from: P, width: Int, height: Int): Region[T]
+  def updated(pos: P, value: T): Block[T]
+  def patch(dest: P, block: Block[T]): Block[T] = block.valuesWithPos.foldLeft(this) {
+    case (acc, (p, v)) => acc.updated(dest + p, v)
+  }
+  def concatX(that: Block[T]): Option[Block[T]]
+  def concatY(that: Block[T]): Option[Block[T]]
+  def flipX: Block[T]
+  def flipY: Block[T]
+  def find(f: T => Boolean): Option[T]
 }
-final case class BlockImpl[T](width: Int, height: Int, values: Seq[T]) extends Block[T]
+final case class BlockImpl[T](width: Int, height: Int, values: Seq[T]) extends Block[T] {
+  override def map[U](f: T => U): Block[U] = BlockImpl(width, height, values.map(f))
+  override def row(index: Int): Option[Row[T]] =
+    if (index < 0 || index >= height) None
+    else Some(RowImpl(values.slice(index * width, (index + 1) * width)))
+  override def region(from: P, width: Int, height: Int): Region[T] =
+    new RegionImpl(this, from, width, height)
+  override def updated(pos: P, value: T): Block[T] =
+    posToIndex(pos).map(i => copy(values = values.updated(i, value))).getOrElse(this)
+  override def concatX(that: Block[T]): Option[Block[T]] =
+    if (height != that.height) None
+    else Some {
+      copy(values = values.grouped(width).zip(that.values.sliding(that.width)).flatMap(xs => xs._1 ++ xs._2).toList)
+    }
+  override def concatY(that: Block[T]): Option[Block[T]] =
+    if (width != that.width) None
+    else Some(copy(values = values ++ that.values))
+  override def flipX: Block[T] = copy(values = values.grouped(width).flatMap(_.reverse).toList)
+  override def flipY: Block[T] = copy(values = values.grouped(width).toList.reverse.flatten)
+  override def find(f: T => Boolean): Option[T] = values.find(f)
+}
 
 class BlockIterator[T](block: Block[T], pos: Int) {
   def hasNext: Boolean = pos < block.size
@@ -19,6 +55,24 @@ class BlockIterator[T](block: Block[T], pos: Int) {
 object Block {
   def empty[T]: Block[T] = BlockImpl(0, 0, Seq.empty)
   def fill[T](width: Int, height: Int, value: => T): Block[T] = BlockImpl(width, height, Seq.fill(width * height)(value))
-  def fill[T](width: Int, value: => T): Block[T] = fill(width, width, value)
+  def fillSquare[T](width: Int, value: => T): Block[T] = fill(width, width, value)
+  def fillWithIndex[T](width: Int, height: Int)(gen: Int => T): Block[T] =
+    BlockImpl(width, height, Seq.tabulate(width * height)(gen))
+  def fillWithPos[T](width: Int, height: Int)(gen: Pos[Int] => T): Block[T] =
+    fillWithIndex(width, height) { i => gen(Pos(i % width, i / width)) }
+  // def fillZero[T](width: Int, height: Int)(implicit z: Zero[T]): Block[T] =
 }
 
+trait Row[T]
+final case class RowImpl[T](values: Seq[T]) extends Row[T]
+
+object Row {
+  def apply[T](values: T*): Row[T] = RowImpl(values)
+}
+
+trait Region[T] {
+  def size: Int
+}
+final class RegionImpl[T](block: Block[T], from: Pos[Int], width: Int, height: Int) extends Region[T] {
+  override def size: Int = width.min(block.width - from.x + 1) * height.min(block.height - from.y + 1)
+}
--- core/src/test/scala/com/yuiwai/yachiyo/core/BlockSpec.scala
+++ core/src/test/scala/com/yuiwai/yachiyo/core/BlockSpec.scala
@@ -10,10 +10,30 @@ object BlockSpec extends TestSuite {
       }
       "fill" - {
         val b = Block.fill(3, 3, false)
-        b == Block.fill(3, false)
+        b == Block.fillSquare(3, false)
         b.size ==> 9
         b.foreach(_ ==> false)
       }
+      "fillWithIndex" - {
+        Block.fillWithIndex(2, 2)(identity).values ==> Seq(0, 1, 2, 3)
+        Block.fillWithIndex(2, 3)(identity).values ==> Seq(0, 1, 2, 3, 4, 5)
+      }
+      "fillWithPos" - {
+        Block.fillWithPos(2, 2)(identity).values ==> Seq(Pos(0, 0), Pos(1, 0), Pos(0, 1), Pos(1, 1))
+      }
+      "fillZero" - {
+        // TODO webglがマージされたら対応
+        // Block.fillZero[Int](2, 2).values ==> Seq(0, 0, 0, 0)
+      }
+    }
+    "values with pos" - {
+      Block.fillWithIndex(2, 2)(identity).valuesWithPos ==>
+        Seq(Pos(0, 0) -> 0, Pos(1, 0) -> 1, Pos(0, 1) -> 2, Pos(1, 1) -> 3)
+    }
+    "map" - {
+      Block.fill(3, 3, 1).map(_ + 1) ==> BlockImpl(3, 3, Seq(2, 2, 2, 2, 2, 2, 2, 2, 2))
+      Block.fill(2, 2, 'a').map(_ + "bcd").values ==> Seq("abcd", "abcd", "abcd", "abcd")
+      Block.fillWithIndex(2, 2)(identity).map(_ * 2).values ==> Seq(0, 2, 4, 6)
     }
     "iterator" - {
       "empty" - {
@@ -21,20 +41,66 @@ object BlockSpec extends TestSuite {
         itr.hasNext ==> false
       }
       "non empty" - {
-      val itr = Block.fill(5, 1).iterator
-      itr.value ==> 1
-      itr.hasNext ==> true
-      itr.next.value ==> 1
+        val itr = Block.fillSquare(5, 1).iterator
+        itr.value ==> 1
+        itr.hasNext ==> true
+        itr.next.value ==> 1
       }
     }
     "row" - {
-
+      val b = Block.fillWithIndex(2, 3)(identity)
+      b.row(-1) ==> None
+      b.row(0) ==> Some(Row(0, 1))
+      b.row(1) ==> Some(Row(2, 3))
+      b.row(2) ==> Some(Row(4, 5))
+      b.row(3) ==> None
     }
     "region" - {
+      val b = Block.fillWithIndex(3, 4)(identity)
+
+      {
+        val r = b.region(Pos(1, 1), 2, 2)
+        r.size ==> 4
+      }
+
+      {
+        val r = b.region(Pos(0, 1), 2, 3)
+        r.size ==> 6
+      }
+
+      {
+        val r = b.region(Pos(3, 3), 2, 2)
+        r.size ==> 2
+      }
     }
-    "update" - {
+    "updated" - {
+      Block.fillSquare(2, 0).updated(Pos(1, 1), 1).values ==> Seq(0, 0, 0, 1)
+      Block.fillSquare(2, 0).updated(Pos(3, 1), 1).values ==> Seq(0, 0, 0, 0)
     }
-    "join" - {
+    "patch" - {
+      Block.fillSquare(3, 0).patch(Pos(0, 0), Block.fill(2, 2, 1)).values ==>
+        Seq(1, 1, 0, 1, 1, 0, 0, 0, 0)
+    }
+    "concat" - {
+      Block.fillSquare(2, 2).concatX(Block.fillSquare(3, 3)) ==> None
+      Block.fillSquare(2, 0).concatX(Block.fill(1, 2, 1)).get.values ==>
+        Seq(0, 0, 1, 0, 0, 1)
+
+      Block.fillSquare(2, 2).concatY(Block.fillSquare(3, 3)) ==> None
+      Block.fillSquare(2, 0).concatY(Block.fill(2, 1, 1)).get.values ==>
+        Seq(0, 0, 0, 0, 1, 1)
+    }
+    "flip" - {
+      Block.fillWithIndex(2, 2)(identity).flipX.values ==> Seq(1, 0, 3, 2)
+      Block.fillWithIndex(2, 2)(identity).flipY.values ==> Seq(2, 3, 0, 1)
+    }
+    "find" - {
+      Block.fillWithIndex(2, 2)(identity).find(_ % 2 == 1) ==> Some(1)
+      Block.fillWithIndex(2, 2)(identity).find(_ == 10) ==> None
+    }
+    "expansion" - {
+    }
+    "shrink" - {
     }
   }
 }

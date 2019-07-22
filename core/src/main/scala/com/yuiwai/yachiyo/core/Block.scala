0867b10d2688b6d52faee19c6a7c6f1e5c42c374
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-07-23 00:41:23 +0900

    チューニング

--- core/src/main/scala/com/yuiwai/yachiyo/core/Block.scala
+++ core/src/main/scala/com/yuiwai/yachiyo/core/Block.scala
@@ -5,10 +5,18 @@ trait Block[T] {
   def width: Int
   def height: Int
   def size: Int = values.size
-  def posToIndex(pos: P): Option[Int] = Some(pos.x + pos.y * width).filter(_ < size)
+  def posToIndex(pos: P): Option[Int] =
+    if (pos.x < 0 || pos.y < 0 || pos.x > width || pos.y > height) None
+    else Some(pos.x + pos.y * width).filter(_ < size)
   def indexToPos(index: Int): P = Pos(index % width, index / width)
   val values: Seq[T]
-  def valuesWithPos: Seq[(P, T)] = values.zipWithIndex.map { case (v, i) => indexToPos(i) -> v }
+  def valuesWithPos: Seq[(P, T)] = {
+    var i = -1
+    values.map { v =>
+      i = i + 1
+      indexToPos(i) -> v
+    }
+  }
   def rows: Seq[Seq[T]] = values.grouped(width).toSeq
   def map[U](f: T => U): Block[U]
   def mapRow[U](f: Seq[T] => Seq[U]): Option[Block[U]]
@@ -18,9 +26,9 @@ trait Block[T] {
   def row(index: Int): Option[Seq[T]]
   def region(from: P, width: Int, height: Int): Region[T]
   def updated(pos: P, value: T): Block[T]
-  def patch(dest: P, block: Block[T]): Block[T] = block.valuesWithPos.foldLeft(this) {
-    case (acc, (p, v)) => acc.updated(dest + p, v)
-  }
+  def patch(dest: P, block: Block[T]): Block[T] = Block.withValues(width, block.valuesWithPos.foldLeft(values) {
+    case (acc, (p, v)) => posToIndex(dest + p).map(i => acc.updated(i, v)).getOrElse(acc)
+  })
   def concatX(that: Block[T]): Option[Block[T]]
   def concatY(that: Block[T]): Option[Block[T]]
   def flipX: Block[T]
@@ -40,7 +48,7 @@ trait Block[T] {
     ): Option[Block[T]]
   def +(that: Block[T])(implicit plus: Plus[T]): Option[Block[T]]
 }
-case class BlockImpl[T](width: Int, height: Int, values: Seq[T]) extends Block[T] {
+case class BlockImpl[T](width: Int, height: Int, values: Vector[T]) extends Block[T] {
   override def map[U](f: T => U): Block[U] = BlockImpl(width, height, values.map(f))
   override def mapRow[U](f: Seq[T] => Seq[U]): Option[Block[U]] = {
     val rows = values.grouped(width).map(f).toSeq
@@ -55,31 +63,32 @@ case class BlockImpl[T](width: Int, height: Int, values: Seq[T]) extends Block[T
   override def region(from: P, width: Int, height: Int): Region[T] =
     new RegionImpl(this, from, width, height)
   override def updated(pos: P, value: T): Block[T] =
-    posToIndex(pos).map(i => copy(values = values.updated(i, value))).getOrElse(this)
+    if (pos.x < 0 || pos.y < 0 || pos.x > width || pos.y > height) this
+    else posToIndex(pos).map(i => copy(values = values.updated(i, value))).getOrElse(this)
   override def concatX(that: Block[T]): Option[Block[T]] =
     if (height != that.height) None
     else Some {
       copy(
         width = width + that.width,
-        values = values.grouped(width).zip(that.values.grouped(that.width)).flatMap(xs => xs._1 ++ xs._2).toList
+        values = values.grouped(width).zip(that.values.grouped(that.width)).flatMap(xs => xs._1 ++ xs._2).toVector
       )
     }
   override def concatY(that: Block[T]): Option[Block[T]] =
     if (width != that.width) None
     else Some(copy(height = height + that.height, values = values ++ that.values))
-  override def flipX: Block[T] = copy(values = values.grouped(width).flatMap(_.reverse).toList)
-  override def flipY: Block[T] = copy(values = values.grouped(width).toList.reverse.flatten)
+  override def flipX: Block[T] = copy(values = values.grouped(width).flatMap(_.reverse).toVector)
+  override def flipY: Block[T] = copy(values = values.grouped(width).toList.reverse.flatten.toVector)
   override def find(f: T => Boolean): Option[T] = values.find(f)
   override def rotateR: Block[T] =
     copy(
       width = height,
       height = width,
-      values = for {x <- 0 until width; y <- 1 to height} yield values(x + (height - y) * width))
+      values = (for {x <- 0 until width; y <- 1 to height} yield values(x + (height - y) * width)).toVector)
   override def rotateL: Block[T] =
     copy(
       width = height,
       height = width,
-      values = for {x <- 1 to width; y <- 0 until height} yield values(width - x + y * width))
+      values = (for {x <- 1 to width; y <- 0 until height} yield values(width - x + y * width)).toVector)
   override def resizeTo(w: Int, h: Int)
     (implicit
       plus: Plus[T],
@@ -111,17 +120,17 @@ class BlockIterator[T](block: Block[T], pos: Int) {
 }
 
 object Block {
-  def empty[T]: Block[T] = BlockImpl(0, 0, Seq.empty)
-  def fill[T](width: Int, height: Int, value: => T): Block[T] = BlockImpl(width, height, Seq.fill(width * height)(value))
+  def empty[T]: Block[T] = BlockImpl(0, 0, Vector.empty)
+  def fill[T](width: Int, height: Int, value: => T): Block[T] = BlockImpl(width, height, Vector.fill(width * height)(value))
   def fillZero[T](width: Int, height: Int)(implicit zero: Zero[T]): Block[T] = fill(width, height, zero())
   def fillSquare[T](width: Int, value: => T): Block[T] = fill(width, width, value)
   def fillWithIndex[T](width: Int, height: Int)(gen: Int => T): Block[T] =
-    BlockImpl(width, height, Seq.tabulate(width * height)(gen))
+    BlockImpl(width, height, Vector.tabulate(width * height)(gen))
   def fillWithPos[T](width: Int, height: Int)(gen: Pos[Int] => T): Block[T] =
     fillWithIndex(width, height) { i => gen(Pos(i % width, i / width)) }
   def fillWithDistance[T](width: Int, height: Int)(gen: Double => T): Block[T] =
     fillWithPos(width, height)(p => gen(Math.sqrt((p.x + .5) * (p.x + .5) + (p.y + .5) * (p.y + .5))))
-  def withValues[T](width: Int, values: Seq[T]): Block[T] = new BlockImpl[T](width, values.size / width, values)
+  def withValues[T](width: Int, values: Seq[T]): Block[T] = new BlockImpl[T](width, values.size / width, values.toVector)
   def resizeX[T: Plus : Minus](width: Int, current: Int, source: Seq[T], dest: Seq[T] = Seq.empty)
     (implicit
       zero: Zero[T],
--- demo/src/main/scala/com/yuiwai/yachiyo/demo/ParticleDemoScene.scala
+++ demo/src/main/scala/com/yuiwai/yachiyo/demo/ParticleDemoScene.scala
@@ -1,10 +1,10 @@
 package com.yuiwai.yachiyo.demo
 
 import com.yuiwai.yachiyo.core._
-import com.yuiwai.yachiyo.demo.ParticleDemoScene.{BackToTop, ParticleDemoState, RequestUpdate, UseBlur}
+import com.yuiwai.yachiyo.demo.ParticleDemoScene.{BackToTop, ParticleDemoState, RequestUpdate}
 import com.yuiwai.yachiyo.ui._
 import org.scalajs.dom
-import org.scalajs.dom.raw.{CanvasRenderingContext2D, HTMLInputElement, ImageData}
+import org.scalajs.dom.raw.{CanvasRenderingContext2D, HTMLElement}
 
 object ParticleDemoScene extends Scene {
 
@@ -12,15 +12,14 @@ object ParticleDemoScene extends Scene {
   override type Command = ParticleDemoMsg
   override type Event = None.type
 
-  final case class ParticleDemoState(system: ParticleSystem[Double], useBlur: Boolean) {
+  final case class ParticleDemoState(system: ParticleSystem[Double]) {
     def updated(): ParticleDemoState = copy(system = system.updated())
   }
   sealed trait ParticleDemoMsg
   case object BackToTop extends ParticleDemoMsg
   case object RequestUpdate extends ParticleDemoMsg
-  final case class UseBlur(flag: Boolean) extends ParticleDemoMsg
 
-  private val lifeTime = 2000
+  private val lifeTime = 500
   private val initialSpeed = Speed(0, -2.0)
   private val initialGravity = Gravity(Force(0, .01))
   private val generatorPos = Pos(150.0, 150.0)
@@ -30,46 +29,42 @@ object ParticleDemoScene extends Scene {
     Seq.empty,
     Generator(s => s.spawn(initialSpeed * Angle.random(-45, 45))),
     initialGravity
-  ), useBlur = false)
+  ))
   override def execute(state: ParticleDemoState, input: ParticleDemoMsg): Result = input match {
     case BackToTop => (state, None, NextSceneCallback(DemoApplication.TopSceneKey))
     case RequestUpdate => (state.updated(), None, NoCallback)
-    case UseBlur(flag) => (state.copy(useBlur = flag), None, NoCallback)
   }
 }
 
 class ParticleDemoPresenter extends Presenter {
+  import ParticleDemoViewModel.{circle, field}
   override type S = ParticleDemoScene.type
   override type M = ParticleDemoViewModel
-  override def usePrevModel: Boolean = true
   override def updated(state: ParticleDemoState, prevModel: Prev): ParticleDemoViewModel = {
-    val additional: Seq[ParticleViewModel] = prevModel.map(_.particles.collect {
-      case p if p.alpha > 0 => p.copy(alpha = p.alpha - 0.1)
-    }).getOrElse(Seq.empty)
-    ParticleDemoViewModel(
-      useBlur = state.useBlur,
-      state.system.particles.map(ParticleViewModel(_)) ++ additional
-    )
+    ParticleDemoViewModel(state.system.particles.foldLeft(field) { case (b, p) =>
+      b.patch(Pos(p.pos.x - circle.width / 2, p.pos.y - circle.height / 2).map(_.toInt), circle)
+    }, state.system.particles.size)
   }
 }
 
-final case class ParticleViewModel(x: Double, y: Double, alpha: Double)
-object ParticleViewModel {
-  def apply(p: Particle[Double]): ParticleViewModel = apply(p.pos.x, p.pos.y, 1.0)
-}
 final case class ParticleDemoViewModel(
-  useBlur: Boolean,
-  particles: Seq[ParticleViewModel]
+  block: Block[Boolean],
+  particleCount: Int
 ) extends ViewModel
-
-class ParticleDemoView extends CanvasView with CommonView {
-  override type S = ParticleDemoScene.type
-  override type M = ParticleDemoViewModel
+object ParticleDemoViewModel {
   val canvasWidth = 300
   val canvasHeight = 300
-  private var prev: Option[ImageData] = None
+  val circle: Block[Boolean] = Drawing.circle(2)
+  val field: Block[Boolean] = Block.fillZero[Boolean](canvasWidth, canvasHeight)
+}
+
+class ParticleDemoView extends CanvasView with CommonView {
+  import ParticleDemoViewModel.{canvasHeight, canvasWidth}
+  override type S = ParticleDemoScene.type
+  override type M = ParticleDemoViewModel
   private var playing = true
   private var ctx2d: Option[CanvasRenderingContext2D] = None
+  private var counter: Option[HTMLElement] = None
   override def setup(viewModel: ParticleDemoViewModel, listener: Listener): Unit = {
     val btn = button("Back To Top")
     btn.onclick = _ => listener(BackToTop)
@@ -79,45 +74,35 @@ class ParticleDemoView extends CanvasView with CommonView {
     ctx2d = Some(canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D])
     container.appendChild(canvas)
 
-    container.appendChild(createElementAs[HTMLInputElement]("input", { e =>
-      e.`type` = "checkbox"
-      e.onclick = { _ => listener(UseBlur(e.checked)) }
-    }))
+    counter = Some(div().tap(e => container.appendChild(e)))
 
     loop(listener)
   }
   override def update(viewModel: ParticleDemoViewModel): Unit = {
-    animation(viewModel.particles, viewModel.useBlur)
+    counter.foreach(_.innerHTML = viewModel.particleCount.toString)
+    draw(viewModel.block)
   }
   override def cleanup(): Unit = {
     super.cleanup()
-    prev = None
     playing = false
   }
-  private def animation(particles: Seq[ParticleViewModel], useBlur: Boolean): Unit = {
+  private def draw(block: Block[Boolean]): Unit = {
     ctx2d.foreach { ctx =>
       ctx.beginPath()
-      ctx.fillStyle = "black"
+      ctx.fillStyle = "rgba(0, 0, 0, .05)"
       ctx.fillRect(0, 0, canvasWidth, canvasHeight)
-      particles.foreach { p =>
-        ctx.beginPath()
-        ctx.fillStyle = s"rgba(255,255,255,${p.alpha})"
-        ctx.arc(p.x, p.y, 5, 0, Math.PI * 2, anticlockwise = false)
-        ctx.fill()
-      }
-
-      if (useBlur) {
-        import com.yuiwai.yachiyo.ui.CanvasView.ImageDataWrap
-        val data = ctx.getImageData(0, 0, canvasWidth, canvasHeight)
-        prev.foreach { pre =>
-          data.pixels
-            .foreach { p =>
-              p.get(pre).foreach(c => p.mod(data, _.mix(c, 1.3)))
-            }
-          ctx.putImageData(data, 0, 0, 0, 0, canvasWidth, canvasHeight)
+      val imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight)
+      var i = 0
+      block.values.foreach { v =>
+        if (v) {
+          imageData.data(i) = 255
+          imageData.data(i + 1) = 255
+          imageData.data(i + 2) = 0
+          imageData.data(i + 3) = 255
         }
-        prev = Some(ctx.getImageData(0, 0, canvasWidth, canvasHeight))
+        i += 4
       }
+      ctx.putImageData(imageData, 0, 0, 0, 0, canvasWidth, canvasHeight)
     }
   }
   private def loop(implicit listener: Listener): Unit = {

c370efa89df8b0b5033ea81cb9d53f2ccd6eb44f
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-11-10 07:54:38 +0900

    Line描画暫定対応

--- drawing/jsCanvas/src/main/scala/com/yuiwai/yachiyo/drawing/js/canvas/Drawing.scala
+++ drawing/jsCanvas/src/main/scala/com/yuiwai/yachiyo/drawing/js/canvas/Drawing.scala
@@ -1,16 +1,18 @@
 package com.yuiwai.yachiyo.drawing.js.canvas
 
 import com.yuiwai.yachiyo.core.{Color, ToDouble}
-import com.yuiwai.yachiyo.drawing.{DrawAction, DrawCircleAction, DrawRectAction, DrawingOps}
+import com.yuiwai.yachiyo.drawing.{DrawAction, DrawCircleAction, DrawLineAction, DrawRectAction, DrawingOps}
 import org.scalajs.dom.raw.CanvasRenderingContext2D
 
 trait Drawing[T] extends DrawingOps[T, CanvasRenderingContext2D] {
-  override def drawLine(fromX: T, fromY: T, toX: T, toY: T): DrawAction[T] = ???
   override def fill(color: Color): DrawAction[T] = ???
   override def stroke(color: Color): DrawAction[T] = ???
   override def execute(action: DrawAction[T])
     (implicit ctx: CanvasRenderingContext2D, d: ToDouble[T]): Unit =
     action match {
+      case DrawLineAction(fromX, fromY, toX, toY) =>
+        ctx.moveTo(d(fromX), d(fromY))
+        ctx.lineTo(d(toX), d(toY))
       case DrawCircleAction(x, y, radius) => {
         ctx.moveTo(d(x), d(y))
         ctx.arc(d(x), d(y), d(radius), 0, Math.PI * 2)

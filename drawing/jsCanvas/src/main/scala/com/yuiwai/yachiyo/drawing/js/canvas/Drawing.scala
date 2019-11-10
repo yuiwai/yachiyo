c3a90bd911ef716ac533af48e4f9f87afeb488ea
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-11-10 22:36:56 +0900

    drawing調整中

--- drawing/jsCanvas/src/main/scala/com/yuiwai/yachiyo/drawing/js/canvas/Drawing.scala
+++ drawing/jsCanvas/src/main/scala/com/yuiwai/yachiyo/drawing/js/canvas/Drawing.scala
@@ -1,12 +1,10 @@
 package com.yuiwai.yachiyo.drawing.js.canvas
 
-import com.yuiwai.yachiyo.core.{Color, ToDouble}
-import com.yuiwai.yachiyo.drawing.{DrawAction, DrawCircleAction, DrawLineAction, DrawRectAction, DrawingOps}
+import com.yuiwai.yachiyo.core.{Color, RGB, ToDouble}
+import com.yuiwai.yachiyo.drawing.{DrawAction, DrawCircleAction, FillStyleAction, DrawLineAction, DrawRectAction, StrokeStyleAction, DrawTextAction, DrawingOps}
 import org.scalajs.dom.raw.CanvasRenderingContext2D
 
 trait Drawing[T] extends DrawingOps[T, CanvasRenderingContext2D] {
-  override def fill(color: Color): DrawAction[T] = ???
-  override def stroke(color: Color): DrawAction[T] = ???
   override def execute(action: DrawAction[T])
     (implicit ctx: CanvasRenderingContext2D, d: ToDouble[T]): Unit =
     action match {
@@ -16,7 +14,22 @@ trait Drawing[T] extends DrawingOps[T, CanvasRenderingContext2D] {
       case DrawCircleAction(x, y, radius) => {
         ctx.moveTo(d(x), d(y))
         ctx.arc(d(x), d(y), d(radius), 0, Math.PI * 2)
+        ctx.closePath()
       }
-      case DrawRectAction(x, y, width, height) => ctx.rect(d(x), d(y), d(width), d(height))
+      case DrawRectAction(x, y, width, height) =>
+        ctx.fillRect(d(x), d(y), d(width), d(height))
+      case DrawTextAction(text, x, y) => ctx.fillText(text, d(x), d(y))
+      case FillStyleAction(color) =>
+        ctx.fillStyle = color.toHexString
+        ctx.fill()
+      case StrokeStyleAction(color) =>
+        ctx.strokeStyle = color.toHexString
+        ctx.stroke()
     }
+
+  implicit class ColorWrap(color: Color) {
+    def toHexString: String = color match {
+      case rgb: RGB => "#" + rgb.toHexString
+    }
+  }
 }
--- drawing/src/main/scala/com/yuiwai/yachiyo/drawing/Drawing.scala
+++ drawing/src/main/scala/com/yuiwai/yachiyo/drawing/Drawing.scala
@@ -6,8 +6,9 @@ trait DrawingOps[T, C] {
   def drawLine(fromX: T, fromY: T, toX: T, toY: T): DrawAction[T] = DrawLineAction(fromX, fromY, toX, toY)
   def drawCircle(x: T, y: T, radius: T): DrawAction[T] = DrawCircleAction(x, y, radius)
   def drawRect(x: T, y: T, width: T, height: T): DrawAction[T] = DrawRectAction(x, y, width, height)
-  def fill(color: Color): DrawAction[T]
-  def stroke(color: Color): DrawAction[T]
+  def drawText(text: String, x: T, y: T): DrawAction[T] = DrawTextAction(text, x, y)
+  def fillStyle(color: Color): DrawAction[T] = FillStyleAction(color)
+  def strokeStyle(color: Color): DrawAction[T] = StrokeStyleAction(color)
   def execute(action: DrawAction[T])(implicit ctx: C, d: ToDouble[T]): Unit
   def execute(actions: Seq[DrawAction[T]])(implicit ctx: C, d: ToDouble[T]): Unit =
     actions foreach execute
@@ -20,3 +21,6 @@ sealed trait DrawAction[T]
 final case class DrawLineAction[T](fromX: T, fromY: T, toX: T, toY: T) extends DrawAction[T]
 final case class DrawCircleAction[T](x: T, y: T, radius: T) extends DrawAction[T]
 final case class DrawRectAction[T](x: T, y: T, width: T, height: T) extends DrawAction[T]
+final case class DrawTextAction[T](text: String, x: T, y: T) extends DrawAction[T]
+final case class FillStyleAction[T](color: Color) extends DrawAction[T]
+final case class StrokeStyleAction[T](color: Color) extends DrawAction[T]
